b8884af39b0ed479780b0127139e5e99264cc688
Annafi Mirza <naffimirza@gmail.com>
2024-10-12 22:15:46 -0400

    Add files via upload
    Updates:
    1. Improved matrix translation to allow for inputs like "x + y = 1" or "-z = 2"
    2. Improved console program in Tester class

--- Matrices/src/matrices/Matrix.java
+++ Matrices/src/matrices/Matrix.java
@@ -3,6 +3,8 @@ package matrices;
 // Remove 1st line & import into IDE
 // only if you want to use file directly
 
+import java.util.ArrayList; // Used for inserting elements in middle of strings
+
 public class Matrix {
 	
 	// INSTANTIATION
@@ -62,7 +64,7 @@ public class Matrix {
 			int coefficientIndex = 0; // Where coefficient begins
 			int coefficientArrayIndex = 0; // Which A(i, j) in matrix A
 				
-			for (int a = 0; a < coefficients.length() - 2; a++) {
+			for (int a = 0; a <= coefficients.length() - 2; a++) {
 				boolean coefficientFound = false;
 				
 				// If current char is an operator except at beginning or immediately after = operator
@@ -86,7 +88,7 @@ public class Matrix {
 				// If current char is =, for operand after =, also a != 0 so no IndexOutOfBoundsException
 				else if ( (a != 0) && (coefficients.substring(a-1, a).equals("=")) ) {
 					coefficientIndex = a;
-					while (coefficientIndex != coefficients.length() - 2) {
+					while (coefficientIndex != coefficients.length() - 1) {
 						coefficient += coefficients.substring(coefficientIndex, coefficientIndex+1);
 						++coefficientIndex;
 					}
@@ -95,6 +97,7 @@ public class Matrix {
 				}
 				// If there exists an operand (coefficient), adds it to matrix and moves to next A(i, j)
 				if (coefficientFound) {
+					coefficient = coefficient.replaceAll(" ", "");
 					outputArray[i][coefficientArrayIndex] = Double.parseDouble(coefficient);
 					++coefficientArrayIndex;
 					coefficient = "";
@@ -112,8 +115,115 @@ public class Matrix {
 		String condensed = line.replaceAll(" ", "") + " ";
 		// ^ Line w/o spaces & extra space at end to prevent bound exceptions
 		
-		int prevVarIndex = 0; // Keep track of last variable
 		boolean xPresent = false; // Sees if x is present or not
+		boolean yPresent = false; // Sees if y is present or not
+		boolean zPresent = false; // Sees if z is present or not
+		for (int i = 0; i < condensed.length() - 1; i++) {
+			String substring = condensed.substring(i, i+1);
+			switch (substring) {
+				case "x":
+					xPresent = true;
+					break;
+				case "y":
+					yPresent = true;
+					break;
+				case "z":
+					zPresent = true;
+					break;
+			}
+		} // ^ Iterates to see which variables are present or not
+		
+		if (!xPresent && !yPresent) { // If equation is z = n
+			if ( condensed.substring(0, 1).equals("-") ) {
+				condensed = "0x+0y" + condensed;
+			} // ^ Adds other variables w/o new operator b/c there already is one
+			else {
+				condensed = "0x+0y+" + condensed;
+			} // ^ Adds other variables w/ + operator
+			xPresent = true;
+			yPresent = true;
+			// ^ All variables are now present; skips to translation
+		}
+		else if (!xPresent && !zPresent) { // If equation is y = n
+			if ( condensed.substring(0, 1).equals("-") ) {
+				condensed = "0x" + condensed;
+			} // ^ Adds x variable w/o new operator b/c there already is one
+			else {
+				condensed = "0x+" + condensed;
+			} // ^ Adds x variable w/ + operator
+			xPresent = true;
+			// ^ x and y variables are present. z is not & will be added later
+		}
+		else if (!yPresent && !zPresent) { // If equation is x = n
+			int variableIndex = 0;
+			for (int i = 0; i < condensed.length() - 1; i++) {
+				if ( condensed.substring(i, i+1).equals("x") ) {
+					variableIndex = i;
+					break;
+				}
+			} // ^ Discovers index of x variable
+			condensed = condensed.substring(0, variableIndex+1) +
+						"+0y+0z" +
+						condensed.substring(variableIndex+1);
+			// ^ New string is everything up to x, the new y & z, and everything after = sign 
+			yPresent = true;
+			zPresent = true;
+			// ^ All variables are now present; skips to translation
+		}
+		
+		if (!xPresent) { // If x is not present
+			if ( condensed.substring(0, 1).equals("-") ) {
+				condensed = "0x" + condensed;
+			}
+			else {
+				condensed = "0x+" + condensed;
+			}
+			// ^ Adds x variable to beginning of string
+			condensed = condensed.substring(0, condensed.length() - 1);
+			// ^ Removes any extra space at end of string
+		}
+		if (!yPresent) { // If y is not present
+			int operandIndex = 0;
+			for (int i = 0; i < condensed.length(); i++) {
+				if ( (i != 0) && ((condensed.substring(i, i+1).equals("+")) || (condensed.substring(i, i+1).equals("-"))) ) {
+					operandIndex = i;
+					break;
+				}
+			} // ^ Finds index of 1st operator not at immediate beginning of string
+			ArrayList<String> newLineArray = new ArrayList<String>();
+			for (int j = 0; j < condensed.length() - 1; j++) {
+				newLineArray.add( condensed.substring(j, j+1) );
+			}
+			newLineArray.add(operandIndex, "+0y");
+			condensed = "";
+			for (String element : newLineArray) {
+				condensed += element;
+			}
+			// ^ Converts string to array, adds y to middle of array, 
+			// & converts back to string
+		}
+		if (!zPresent) { // If z is not present
+			int operandIndex = 0;
+			for (int i = 0; i < condensed.length(); i++) {
+				if ( (condensed.substring(i, i+1).equals("=")) ) {
+					operandIndex = i;
+					break;
+				}
+			} // ^ Finds index of = sign
+			ArrayList<String> newLineArray = new ArrayList<String>();
+			for (int j = 0; j < condensed.length() - 1; j++) {
+				newLineArray.add( condensed.substring(j, j+1) );
+			}
+			newLineArray.add(operandIndex, "+0z");
+			condensed = "";
+			for (String element : newLineArray) {
+				condensed += element;
+			}
+			// ^ Converts string to array, adds z to middle of array, 
+			// & converts back to string
+		}
+		
+		int prevVarIndex = 0; // Keep track of last variable
 		for (int i = 0; i < condensed.length() - 1; i++) {
 			String substring = condensed.substring(i, i + 1); // Each char
 			
@@ -122,12 +232,15 @@ public class Matrix {
 					if (i == 0) { // No coefficients
 						newLine += "1";
 					}
-					else if (i == 1) { // 0 or -1 as coefficient
+					else if (i == 1) { // 0, -1, or single-digit integer as coefficient
 						if ( condensed.substring(0, 1).equals("0") ) {
 							newLine += "0";
 						}
-						else {
+						else if ( condensed.substring(0, 1).equals("-") ) {
 							newLine += "-1";
+						}
+						else {
+							newLine += condensed.substring(0, 1);
 						}
 					}
 					else { // Any other coefficient
@@ -153,21 +266,7 @@ public class Matrix {
 							++beginning;
 						}
 					}
-					prevVarIndex = i; // Index of y
-					
-					/** (W.I.P.)
-					// In case x is not present:
-					if (!(xPresent)) {
-						// Inserts 0 for x at beginning of newLine
-						StringBuilder str = new StringBuilder(newLine);
-						str.insert(0, '0');
-						if ( !(newLine.substring(0, 1).equals("-")) ) {
-							str.insert(1, '+');
-						}
- 						newLine = str.toString();
-					}
-					**/
-					
+					prevVarIndex = i; // Index of y				
 					break;
 					
 				case "z":
--- Matrices/src/matrices/MatrixTester.java
+++ Matrices/src/matrices/MatrixTester.java
@@ -7,47 +7,154 @@ import java.util.Scanner;
 
 public class MatrixTester {
 	
-	public static void main(String[] args) {
+	static Scanner in = new Scanner(System.in); // Scans user input
+	static Matrix matrixA; // User's matrix
 	
-		Scanner in = new Scanner(System.in); // Scans user input
-		System.out.println("Hello! Welcome to the world of matrices.\n"
-				+ 			"Enter 3 equations to form a system of\n"
+	// MAKING MATRICES
+	
+	// If the user wants to create a square matrix for determinants
+	public static void createSquareMatrix() {
+		
+	} // End of createSquareMatrix method
+	
+	// If the user wants to create an augmented matrix
+	public static void createAugmentedMatrix() {
+		
+		System.out.println("\nEnter 3 equations to form a system of\n"
 				+ 			"equations. Acceptable forms include:\n"
-				+ 			"x + 2y + 3z = 0\n"
-				+ 			"-x + 0y - z = 2\n"
+				+ 			"3x - 4.6y + 234z = -339\n"
+				+ 			"-23x + 4z = 3\n"
+				+			"-y = -1"
 				+ 			"0x + y + 0z = 7\n"
 				+ 			"x-y+2z=4\n");
+		
+		String line1 = "";
+		String line2 = "";
+		String line3 = "";
+		
+		System.out.print("Equation 1: ");
+		line1 += in.next() + in.nextLine();
+		System.out.println("Equation 1: " + line1 + "\n");
+		
+		System.out.print("Equation 2: ");
+		line2 += in.next() + in.nextLine();
+		System.out.println("Equation 2: " + line2 + "\n");
+		
+		System.out.print("Equation 3: ");
+		line3 += in.next() + in.nextLine();
+		System.out.println("Equation 3: " + line3 + "\n");
+		
+		matrixA = new Matrix(line1, line2, line3);
+		System.out.println("\n\nCool, here's your matrix!\n" + matrixA);
+	
+	} // End of createAugmentedMatrix method
+	
+	// MATRIX FUNCTIONS
+	
+	// Gets RRE form of matrix (solutions to system of equations) if any
+	public static void getRREForm() {
+		System.out.println("Loading RRE form...\n");
+	} // End of getRREForm method
+	
+	// Gets inverse of matrix if possible
+	public static void getInverse() {
+		System.out.println("Loading inverse...\n");
+	} // End of getInverse method
+	
+	// Gets determinant of matrix
+	public static void getDeterminant() {
+		System.out.println("Loading determinant...\n");
+	} // End of getDeterminant method
+	
+	// MAIN METHOD
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello! Welcome to the world of matrices.");
 		// ^ Introduction
 		
 		// Console program for all matrix operations
 		while (true) {
 			
-			String line1 = "";
-			String line2 = "";
-			String line3 = "";
+			String userMatrixChoice; // Keeps user's choice for what to do w/ matrix
 			
+			// Allows user to exit program
 			System.out.print("Do you want to continue or quit? (1/2) ");
 			String continueKey = in.next();
 			if (continueKey.equals("2")) {
 				System.out.print("See you soon!");
 				break;
 			}
-			System.out.println("\nCool! Enter each line of your matrix -\n");
 			
-			System.out.print("Equation 1: ");
-			line1 += in.next() + in.nextLine();
-			System.out.println("Equation 1: " + line1 + "\n");
+			// Prompts the user to create a matrix
+			System.out.print("\nFirst, do you want to create a square\n"
+					+ 			"matrix or an augmented matrix? (1/2) ");
+			userMatrixChoice = in.next();
+			boolean properlyDone = false;
+			while (!properlyDone) {
+				switch(userMatrixChoice) {
+					case "1":
+						createSquareMatrix();
+						properlyDone = true;
+						break;
+					case "2":
+						createAugmentedMatrix();
+						properlyDone = true;
+						break;
+					default:
+						System.err.println("Not a valid option!");
+						userMatrixChoice = in.next();
+				}
+			} // End of creating matrix
 			
-			System.out.print("Equation 2: ");
-			line2 += in.next() + in.nextLine();
-			System.out.println("Equation 2: " + line2 + "\n");
-			
-			System.out.print("Equation 3: ");
-			line3 += in.next() + in.nextLine();
-			System.out.println("Equation 3: " + line3 + "\n");
-			
-			Matrix matrixA = new Matrix(line1, line2, line3);
-			System.out.println("\n\nCool, here's your matrix!\n" + matrixA);
+			// Prompts user to manipulate matrix in different ways
+			properlyDone = false;
+			// If user made a square matrix
+			if (userMatrixChoice.equals("1")) {
+				System.out.println("\nNow that you have your square matrix,\n"
+						+ 		   "what do you want to do with it? (1-2)\n"
+						+ 		   "(1) > Find inverse of matrix if possible.\n"
+						+ 		   "(2) > Find determinant of matrix.");
+				userMatrixChoice = in.next();
+				while (!properlyDone) {
+					switch(userMatrixChoice) {
+						case "1":
+							getInverse();
+							properlyDone = true;
+							break;
+						case "2":
+							getDeterminant();
+							properlyDone = true;
+							break;
+						default:
+							System.err.println("Not a valid option!");
+							userMatrixChoice = in.next();
+					}
+				} // End of manipulating square matrix
+			}
+			// If user made an augmented matrix
+			else {
+				System.out.print("Do you want to reduce your matrix to RRE form\n"
+						+ 		 "and, thus, gain the solutions (if any) to your\n"
+						+ 		 "system? (Yes/No, 1/2) ");
+				userMatrixChoice = in.next();
+				while (!properlyDone) {
+					switch(userMatrixChoice) {
+					case "1":
+						getRREForm();
+						properlyDone = true;
+						break;
+					case "2":
+						System.out.println("Okay!\n");
+						properlyDone = true;
+						break;
+					default:
+						System.err.println("Not a valid option!");
+						userMatrixChoice = in.next();
+					}
+				}
+			}
+			// End of user manipulation of matrix
 			
 		} // End of console program (while loop)
 	
